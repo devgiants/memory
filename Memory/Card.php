91634d82cf5c73ef4bd23b61e3f01bc1b767f5cd
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-12-20 22:34:58 +0100

    Add Card class

---
+++ Memory/Card.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolas
+ * Date: 20/12/18
+ * Time: 21:30
+ */
+
+namespace Memory;
+
+/**
+ * Class Card
+ *
+ * @package Memory
+ */
+class Card
+{
+
+}
